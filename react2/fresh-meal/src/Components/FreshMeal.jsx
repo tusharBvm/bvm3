34677269ea621fdfcb9bdf3da87748b487a16599
tusharBvm <tushar.bvminfotech@gmail.com>
2025-08-21 17:59:20 +0530

    ui cmp

--- react2/fresh-meal/src/Components/FreshMeal.jsx
+++ react2/fresh-meal/src/Components/FreshMeal.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import logo from "../Assets/images/logo.png";
-import meal from "../Assets/images/meal-img.jpg";
+import meal2 from "../Assets/images/bistro4-slider-pic1.jpeg";
 import bistro from "../Assets/images/bistro4-about-pic1.jpg";
 import eatting from "../Assets/images/eatting.jpg";
 import dishes from "../Assets/images/dishes.jpg";
 import story from "../Assets/images/story.jpg";
+import logo2 from "../Assets//images/logo2.png";
+import dish1 from "../Assets/images/dish1.jpg";
+import dish2 from "../Assets/images/dish2.jpg";
+import dish3 from "../Assets/images/dish3.jpg";
+import dish4 from "../Assets/images/dish4.jpg";
+import dish5 from "../Assets/images/dish5.jpg";
+import dish6 from "../Assets/images/dish6.jpg";
+import gallery from "../Assets/images/gallery.jpg";
 import { IoLocationSharp } from "react-icons/io5";
-import { FaDownload } from "react-icons/fa";
+import { FaDownload, FaImages } from "react-icons/fa";
 import { MdKeyboardArrowDown } from "react-icons/md";
 
 function FreshMeal() {
   return (
     <>
       {/* header start */}
-      <div className="container-fluid header-cnt px-4">
+      <div className="container-fluid border-btm header-cnt header px-4">
         <div className="logo">
           <a href="">
             <img src={logo} alt="" />
@@ -27,16 +35,19 @@ function FreshMeal() {
                   <a class="nav-link spacing fw-bold" href="#">
                     Start
                   </a>
-                  <a class="nav-link spacing fw-bold" href="#">
+                  <a class="nav-link spacing fw-bold" href="#about">
                     About
                   </a>
-                  <a class="nav-link spacing fw-bold" href="#">
+                  <a class="nav-link spacing fw-bold" href="#menu">
+                    Menu
+                  </a>
+                  <a class="nav-link spacing fw-bold" href="#story">
                     Our story
                   </a>
-                  <a class="nav-link spacing fw-bold" href="#">
+                  <a class="nav-link spacing fw-bold" href="#speical">
                     Our specials
                   </a>
-                  <a class="nav-link spacing fw-bold" href="#">
+                  <a class="nav-link spacing fw-bold" href="#contact">
                     Contact
                   </a>
                   <a class="nav-link spacing fw-bold" href="#">
@@ -48,7 +59,7 @@ function FreshMeal() {
           </nav>
         </div>
       </div>
-      {/* header end */}
+      {/* header end  */}
 
       {/* img-cnt  start*/}
       <div className="container-fluid d-flex p-0 main bg">
@@ -60,26 +71,26 @@ function FreshMeal() {
               <span style={{ color: "white" }}>7 AM - 8 PM</span>
             </p>
             <button className="rounded-pill read-more">
-              <a href="" className="readmore-a">
+              <a href="#about" className="readmore-a">
                 Read more
               </a>
             </button>
           </div>
         </div>
         <div className="w-50">
-          <img className="img-set" src={meal} alt="" />
+          <img className="img-set" src={meal2} alt="" />
         </div>
       </div>
       {/* img-cnt end*/}
 
       {/* best-bistro start */}
-      <div className="container-fluid space">
+      <div className="container-fluid space" id="about">
         <div className="container">
           <div class="row ">
-            <div class="col-6 space-2">
-              <img className="bistro-img" src={bistro} alt="" />
+            <div class="col-md-6 col-12 space-2">
+              <img className="bistro-img " src={bistro} alt="" />
             </div>
-            <div class="col-6 space-2">
+            <div class="col-md-6 col-12 space-2">
               <div className="bistro-cnt mt-55">
                 <div className="mb-40">
                   <p>Our Location</p>
@@ -120,7 +131,7 @@ function FreshMeal() {
           </div>
 
           <div className="row p-100">
-            <div className="col-3">
+            <div className=" col-md-3 col-12 ">
               <div className="eating-cnt mt-55">
                 <div className="mb-40">
                   <h2>Ut ultricies imperdiet</h2>
@@ -140,10 +151,10 @@ function FreshMeal() {
                 </div>
               </div>
             </div>
-            <div className="col-6">
+            <div className="col-md-6 col-12">
               <img src={eatting} alt="" className="bistro-img" />
             </div>
-            <div className="col-3">
+            <div className=" col-md-3 col-12">
               <div className="eating-cnt align mt-55 ">
                 <div className="mb-40">
                   <h2>Mauris accumsan</h2>
@@ -166,7 +177,7 @@ function FreshMeal() {
           </div>
           <div className="text-center">
             <button className="rounded-pill menu-btn">
-              <a href="">See Our Menu</a>
+              <a href="#menu">See Our Menu</a>
             </button>
           </div>
         </div>
@@ -176,10 +187,10 @@ function FreshMeal() {
       <div className="box"></div>
 
       {/* selected dishes start */}
-      <div className="container-fluid space bg-green">
+      <div className="container-fluid space bg-green" id="menu">
         <div className="container">
           <div className="row ">
-            <div className="col-6 pd">
+            <div className="col-md-6 col-12 pd">
               <div className="dishes-cnt mt-55">
                 <div className="mb-40">
                   <p>Selected</p>
@@ -233,8 +244,8 @@ function FreshMeal() {
                 </div>
               </div>
             </div>
-            <div className="col-6">
-              <img className="bistro-img" src={dishes} alt="" />
+            <div className="col-md-6 col-12">
+              <img className="bistro-img mt-130" src={dishes} alt="" />
             </div>
           </div>
         </div>
@@ -242,7 +253,7 @@ function FreshMeal() {
       {/* selected dishes end */}
 
       {/* story start */}
-      <div className="container-fluid bg-story ">
+      <div className="container-fluid bg-story " id="story">
         <div className="container">
           <div className="row">
             <div className="col-6 space">
@@ -273,24 +284,121 @@ function FreshMeal() {
               <div>
                 <img src={story} className="bistro-img" alt="" />
               </div>
-                <div className="eating-cnt mt-55  col-6 ">
+              <div className="row">
+                <div className="col-4"></div>
+                <div className="story-cnt2 col-6 ">
                   <div className="mb-40">
                     <h2>1997</h2>
                     <span className="eating-span">
-                      Aliquam fringilla aliquam ex sit amet elementum. Proin bibendum.
+                      Aliquam fringilla aliquam ex sit amet elementum. Proin
+                      bibendum.
                     </span>
                   </div>
 
                   <hr />
 
-                  <div className="mb-40 mt-36">
-                    <h2>168</h2>
-                    <span className="eating-span">
-                      Integer magna purus, commodo in diam nec, pretium auctor.
-                    </span>
-                  </div>
+                  <h2>168</h2>
+                  <span className="eating-span">
+                    Integer magna purus, commodo in diam nec, pretium auctor.
+                  </span>
                 </div>
-              
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* story end */}
+
+      {/* special start  */}
+      <div className="container-fluid space" id="speical">
+        <div className="container">
+          <div className="row ">
+            <div className="col-9">
+              <p className="speicial-p">What we offer</p>
+              <h1 className="special-h1">Our specials</h1>
+            </div>
+            <div className="col-3">
+              <img src={logo2} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* col-xxl-4 col-xl-4 col-lg-4 col-md-12 col-sm-12 */}
+
+      <div className="container-fluid pb-80">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4 col-12">
+              <img src={dish1} alt="" style={{ width: "100%" }} />
+              <p className="dish-p">Aliquam fringilla aliquam</p>
+            </div>
+            <div className="col-md-4  col-12">
+              <img src={dish2} alt="" style={{ width: "100%" }} />
+              <p className="dish-p">Lorem ipsum </p>
+            </div>
+            <div className="col-md-4 col-12">
+              <img src={dish3} alt="" style={{ width: "100%" }} />
+              <p className="dish-p">Nulla mauris dolor </p>
+            </div>
+            <div className="col-md-4 col-12 mt-3">
+              <img src={dish4} alt="" style={{ width: "100%" }} />
+              <p className="dish-p">Curabitur rutrum</p>
+            </div>
+            <div className="col-md-4 col-12 mt-3">
+              <img src={dish5} alt="" style={{ width: "100%" }} />
+              <p className="dish-p">Class aptent taciti</p>
+            </div>
+            <div className="col-md-4 col-12 mt-3">
+              <img src={dish6} alt="" style={{ width: "100%" }} />
+              <p className="dish-p">Maecenas sit amet</p>
+            </div>
+          </div>
+          <div className="text-center mt-5">
+            <button className="rounded-pill menu-btn">
+              <a href="">Gallery</a>
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* specal-end */}
+
+      {/* photos Start */}
+      <div className="container-fluid bg-story space">
+        <div className="container">
+          <div className="text-center">
+            <h4 className="theme">BE BISTRO</h4>
+            <h1 className="special-h1">
+              See our photos <br /> on instagram
+            </h1>
+            <img className="gallery-img mt-55" src={gallery} alt="" />
+            <button className="rounded-pill menu-btn mt-5">
+              <a href="">
+                <FaImages /> &nbsp; View our instagram
+              </a>
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* photos end */}
+
+      {/* footer start */}
+      <div className="container-fluid space footer-bg" id="contact">
+        <div className="container">
+          <img src={logo2} alt="" />
+          <h1 className="footer-h1 mt-3">Contact us</h1>
+          <div className="row">
+            <div className="col-3">
+              <p className="footer-p mt-5">
+                Level 13, 2 Elizabeth St, Melbourne, Victoria 3000, Australia
+              </p>
+              <p className="footer-p mt-5">Everyday: 7 AM - 8 PM</p>
+            </div>
+            <div className="col-4">
+              <p className="footer-p mt-5">Phone</p>
+              <h2 className="footer-h2">+61 (0) 3 8376 6284</h2>
+              <p className="footer-p mt-5">E-mail</p>
+              <p className="footer-p2">noreply@envato.com</p>
             </div>
           </div>
         </div>
